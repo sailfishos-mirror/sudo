f833438f0855efc0c5a3be94b42ad98f385f7489
Todd C. Miller <Todd.Miller@sudo.ws>
2026-04-27 11:20:39 -0600

    ts_match_record: allow sub-second difference in session start time
    The session leader start time is wall clock time and can be affected
    by ntp clock updates.  This can result in the time stamp entry being
    ignored when the clock is updated.  Since small clock adjustments
    are common, we now allow sub-second fluctuations in the session
    start time.
    
    We want to limit the possibility of the user logging out and back
    in again and recycling the same tty session so large clock adjustments
    will still invalidate the time stamp record.

--- plugins/sudoers/timestamp.c
+++ plugins/sudoers/timestamp.c
@@ -124,9 +124,21 @@ ts_match_record(struct timestamp_entry *key, struct timestamp_entry *entry,
 	    debug_return_bool(false);
 	}
 	if (sudo_timespeccmp(&entry->start_time, &key->start_time, !=)) {
-	    sudo_debug_printf(SUDO_DEBUG_DEBUG,
-		"%s:%u session leader start time mismatch", __func__, recno);
-	    debug_return_bool(false);
+	    /*
+	     * Session leader start time is wall clock time and can be
+	     * affected by ntp clock updates.  We only allow sub-second
+	     * fluctuations to limit the possibility of the user logging
+	     * out and back in again and recycling the same tty session.
+	     */
+	    int64_t diff = 1000000000L *
+		(int64_t)(entry->start_time.tv_sec - key->start_time.tv_sec);
+	    diff += entry->start_time.tv_nsec - key->start_time.tv_nsec;
+	    if (diff > 1000000000L || diff < -1000000000L) {
+		sudo_debug_printf(SUDO_DEBUG_DEBUG,
+		    "%s:%u session leader start time mismatch (%lld nsec)",
+		    __func__, recno, (long long)diff);
+		debug_return_bool(false);
+	    }
 	}
 	break;
     default:
